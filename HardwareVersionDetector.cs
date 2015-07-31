6b70a7433b7724f67844f1e1a3a31ca6dbf6d59d
Andrius <AndriusMacbook@MacBook-Pro.local>
2015-07-31 15:54:36 +0300

    Initial commit

---
+++ HardwareVersionDetector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ObjCRuntime;
+using System.Runtime.InteropServices;
+
+namespace Transparency
+{
+	public static class HardwareVersionDetector
+	{
+		private const string HardwareProperty = "hw.machine";
+
+		[DllImport(Constants.SystemLibrary)]
+		internal static extern int sysctlbyname([MarshalAs(UnmanagedType.LPStr)] string property,
+			IntPtr output,
+			IntPtr oldLen,
+			IntPtr newp,
+			uint newlen
+		);
+
+		public static string HardwareVersion
+		{
+			get
+			{
+				try 
+				{
+					// get the length of the string
+					var pLen = Marshal.AllocHGlobal(sizeof(int));
+					sysctlbyname(HardwareProperty, IntPtr.Zero, pLen, IntPtr.Zero, 0);
+
+					var length = Marshal.ReadInt32(pLen);
+
+					// check to see if we got a length
+					if (length == 0)
+					{
+						Marshal.FreeHGlobal(pLen);
+						return "Unknown";
+					}
+
+					// get the hardware string
+					var pStr = Marshal.AllocHGlobal(length);
+					sysctlbyname(HardwareProperty, pStr, pLen, IntPtr.Zero, 0);
+
+					// convert the native string into a C# string
+					var hardwareStr = Marshal.PtrToStringAnsi(pStr);
+
+					// cleanup
+					Marshal.FreeHGlobal(pLen);
+					Marshal.FreeHGlobal(pStr);
+
+					return hardwareStr;
+				} 
+				catch (Exception ex) 
+				{
+					Console.WriteLine("HardwareVersionDetector.HardwareVersion Ex: " + ex.Message);
+				}
+
+				return "Unknown";
+			}
+		}
+	}
+}
+
---
+++ TransparencyDetector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Transparency
+{
+	public static class TransparencyDetector
+	{
+		private static int iOSVersionMajor = UIDevice.CurrentDevice.SystemVersion[0] - '0'; // Convert char to int
+
+		public static bool IsTransparencyAvailable (bool defaultValue = true) // defaultValue is used when running on simulator
+		{
+			//There is no way to determine device on simulator. Return defaultValue instead
+			if(ObjCRuntime.Runtime.Arch == ObjCRuntime.Arch.SIMULATOR)
+			{
+				return defaultValue;
+			}
+
+			// Blur effect is only available on iOS 7 and above.
+			if(iOSVersionMajor < 7)
+			{
+				return false;
+			}
+
+			if(iOSVersionMajor >= 8)
+			{
+				// Check if Reduce Transparency option is enabled in Settings.
+				// This feature is available since iOS 7 but this property exists only in iOS 8 and up. Yes, strange :)
+				if (UIAccessibility.IsReduceTransparencyEnabled)
+				{
+					return false;
+				}
+			}
+
+			//Lastly we check if device is one of the supported devices by Apple.
+			return DoesDeviceSupportTransparency();
+		}
+
+
+		private static bool DoesDeviceSupportTransparency()
+		{
+			string[] unsupportedDevices = 
+			{	"Unknown",
+				"iPad",
+				"iPad1,1",
+				"iPhone1,1",
+				"iPhone1,2",
+				"iPhone2,1",
+				"iPhone3,1",
+				"iPhone3,2",
+				"iPhone3,3",
+				"iPod1,1",
+				"iPod2,1",
+				"iPod2,2",
+				"iPod3,1",
+				"iPod4,1",
+				"iPad2,1",
+				"iPad2,2",
+				"iPad2,3",
+				"iPad2,4",
+				"iPad3,1",
+				"iPad3,2",
+				"iPad3,3" };
+
+			int index = Array.IndexOf(unsupportedDevices, HardwareVersionDetector.HardwareVersion);
+
+			return (index == -1);
+		}
+	}
+}
+
